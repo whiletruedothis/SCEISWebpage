457213c44fa18c81a8f82b4852b2ee71e27b1a97
Daniel A <daniel.aparicio.q@gmail.com>
2019-02-11 07:45:59 -0400

    Configure environment and install firebase packages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA0cWcsM0KS5dnCmV7th4vFZUzIDSTazd0',
+    authDomain: 'sceis-web.firebaseapp.com',
+    databaseURL: 'https://sceis-web.firebaseio.com',
+    projectId: 'sceis-web',
+    storageBucket: 'sceis-web.appspot.com',
+    messagingSenderId: '915095390741'
+  }
 };
 
 /*
