657d7d4046b7aca9252e7b5ad2bf85dbfda5c52b
3aVarana <3av.arana@gmail.com>
2019-02-12 23:38:11 -0400

    navbar and footer added

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
+// Components
+import { NavbarComponent } from './navbar/navbar.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [NavbarComponent, WrapperComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    BrowserModule
+  ],
+  exports: [
+    WrapperComponent
+  ]
+})
+export class CoreModule { }
