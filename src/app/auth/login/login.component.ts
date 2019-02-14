c19469dc260e4c7058f47cddc2330703800d31e0
Daniel A <daniel.aparicio.q@gmail.com>
2019-02-13 20:55:42 -0400

    Add validators and effect to button

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -18,8 +18,8 @@ export class LoginComponent implements OnInit {
 
   initializeForm() {
     this.loginForm = this.fb.group({
-      email: [ '', [Validators.required] ],
-      password: [ '', [Validators.required] ]
+      email: [ '', [ Validators.required, Validators.pattern('[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}') ] ],
+      password: [ '', [ Validators.required, Validators.minLength(6) ] ]
     });
   }
 
