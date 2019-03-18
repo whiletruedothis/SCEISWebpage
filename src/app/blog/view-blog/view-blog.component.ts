ada0bb6a7bae83cf2361fb02fd51205f89795679
Daniel A <daniel.aparicio.q@gmail.com>
2019-03-17 22:05:00 -0400

    Add button to edit info

--- src/app/blog/view-blog/view-blog.component.ts
+++ src/app/blog/view-blog/view-blog.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService } from 'src/app/shared/services/blog.service';
 import { Blog } from 'src/app/shared/models/blog';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/auth.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-view-blog',
@@ -11,16 +13,33 @@ import { ActivatedRoute } from '@angular/router';
 export class ViewBlogComponent implements OnInit {
 
   blog: any;
+  isAdmin: boolean;
 
-  constructor(private blogService: BlogService, private route: ActivatedRoute) {
+  constructor(private blogService: BlogService, private route: ActivatedRoute, private authService: AuthService,
+              private router: Router) {
     this.blog = new Blog();
+    this.isAdmin = false;
   }
 
   ngOnInit() {
     this.blogService.getBlogById(this.route.snapshot.params.id).subscribe( result => {
       this.blog = result.payload.data();
     });
+    this.isLogged();
+  }
 
+  isLogged() {
+    this.authService.isLogged().subscribe(user => {
+      if (user) {
+        this.isAdmin = true;
+      } else {
+        this.isAdmin = false;
+      }
+    });
+  }
+
+  editBlog() {
+    this.router.navigate(['']);
   }
 
 }
